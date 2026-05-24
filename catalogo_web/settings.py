e425d1b57e06261eacaf0d25c1b3084c356336df
fati1 <fm2290759@gmail.com>
2026-05-23 20:28:15 -0600

    Templates personalizados

--- catalogo_web/settings.py
+++ catalogo_web/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'catalogo_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'sistema/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
