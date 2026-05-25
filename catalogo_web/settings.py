4045ae37034f69d5a4c9a49f2a67eacbc9c5bbfc
fati1 <fm2290759@gmail.com>
2026-05-24 18:49:40 -0600

    sendgrid smtp

--- catalogo_web/settings.py
+++ catalogo_web/settings.py
@@ -88,15 +88,17 @@ DATABASES = {
 }
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_HOST = 'smtp.sendgrid.net'
 
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'apikey'
+
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = 'fm2290759@gmail.com'
 
 EMAIL_TIMEOUT = 30
 
