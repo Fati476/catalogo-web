0a4aa070f58efcd8f18a4b37c15760cc526626a0
fati1 <fm2290759@gmail.com>
2026-05-24 18:18:16 -0600

    configuracion correo brevo

--- catalogo_web/settings.py
+++ catalogo_web/settings.py
@@ -89,16 +89,16 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp-relay.brevo.com'
-
 EMAIL_PORT = 587
-
 EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+EMAIL_TIMEOUT = 30
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
