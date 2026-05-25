d8521a58ca3c52af0a29f09d0df8db78399df11a
fati1 <fm2290759@gmail.com>
2026-05-24 18:10:56 -0600

    Configurar Brevo SMTP

--- catalogo_web/settings.py
+++ catalogo_web/settings.py
@@ -88,13 +88,11 @@ DATABASES = {
 }
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp-relay.brevo.com'
 
 EMAIL_PORT = 587
 
 EMAIL_USE_TLS = True
-
-EMAIL_TIMEOUT = 30
 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 
