19f5e5b77685eefe7420907eaca9564d2d56d0fe
fati1 <fm2290759@gmail.com>
2026-05-25 19:52:13 -0600

    registro2

--- catalogo_web/settings.py
+++ catalogo_web/settings.py
@@ -95,18 +95,7 @@ DEFAULT_FROM_EMAIL = 'fm2290759@gmail.com'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    
 ]
 
 
