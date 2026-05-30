91ab28143f2ae53702e3ed931560d98569524188
fati1 <fm2290759@gmail.com>
2026-05-29 20:05:10 -0600

    Administrador35

--- catalogo_web/settings.py
+++ catalogo_web/settings.py
@@ -100,7 +100,7 @@ load_dotenv()
 cloudinary.config(
     cloud_name=os.getenv('CLOUD_NAME'),
     api_key=os.getenv('API_KEY'),
-    api_secret=os.getenv('API_SECRET')
+    api_secret=os.getenv('API_SECRET'),
     secure=True
 )
 
