1b9520e7881dcf1d397a93c0cc7d3d57212463f9
fati1 <fm2290759@gmail.com>
2026-05-30 20:02:24 -0600

    Administrador49

--- sistema/models.py
+++ sistema/models.py
@@ -16,6 +16,11 @@ class Producto(models.Model):
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
 
+    estado = models.BooleanField(
+        default=True,
+        verbose_name="Activo"
+    )
+
     def __str__(self):
         return self.nombre
 
